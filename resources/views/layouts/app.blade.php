a48258f5cf1224a161eea3dd7b47796b71c11df2
kurkotamas <kurkotamas@gmail.com>
2019-07-05 00:54:25 +0300

    about section instruments creator card with profile pictures

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -159,20 +159,23 @@
 <section id="about-section" class="py-5">
     <div class="container">
         <div class="row">
-            <div class="col-md-6 order-2">
-                <img src="images/man.jpg" alt="" class="img-fluid mb-3 rounded-circle">
-            </div>
-            <div class="col-md-6 order-1">
-                <h3>Create Your Passion</h3>
-                <p>@yield('about_head_text_bottom')</p>
-                <div class="d-flex">
-                    <div class="p-4 align-self-start">
-                        <i class="fas fa-check fa-2x"></i>
-                    </div>
-                    <div class="p-4 align-self-end">
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nulla porro aperiam, quibusdam at esse voluptate!
+            <div class="col-md-4 order-2">
+                <div class="card">
+                    <div class="card-body text-dark text-center">
+                       <img src="images/person4.jpg" alt="" class="img-fluid rounded-circle">
+                        <div class="card-title">
+                            <h3>Antal Attila</h3>
+                        </div>
+                        <div class="card-body">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, vitae, voluptates? Perferendis, obcaecati placeat est.
+                        </div>
                     </div>
                 </div>
+                
+            </div>
+            <div class="col-md-8 order-1">
+                <h3>Create Your Passion</h3>
+                <p>@yield('about_head_text_bottom')</p>
                 <div class="d-flex">
                     <div class="p-4 align-self-start">
                         <i class="fas fa-check fa-2x"></i>
